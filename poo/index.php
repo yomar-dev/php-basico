d02b5c8e0d8482b225339376c677709df8724177
Yomar Miranda <mirandayomar@gmail.com>
2018-02-03 17:40:01 -0500

    Ahora hemos utilizado el metodo 'constructor' para darle valor a una propiedad, en este caso el color del Carro, además hemos creado un metodo que nos permite obtener el color de dicho Carro.

--- poo/index.php
+++ poo/index.php
@@ -3,6 +3,11 @@
 class Carro{
 	
 	private $propietario;
+	private $color;	
+
+	public function __construct($color){
+		$this->color = $color;
+	}
 
 	public function mover(){
 		echo "Carro en movimiento<br>";
@@ -15,14 +20,19 @@ class Carro{
 	public function setPropietario($nombre){
 		$this->propietario = $nombre;
 	}
+
+	public function getColor(){
+		return $this->color;
+	}
 }
 
 
 echo "<h3>Clase Carro</h3>";
 
-$miCarro = new Carro();
+$miCarro = new Carro("Negro");
 $miCarro->mover();
 $miCarro->setPropietario("Cody");
 echo "<strong>Propietario: </strong>" . $miCarro->getPropietario();
+echo "<br><strong>Color: </strong>" . $miCarro->getColor();
 
 ?>
