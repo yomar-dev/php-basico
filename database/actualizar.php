936432686af938c352878151c6a0289f4a1b1d21
Yomar Miranda <mirandayomar@gmail.com>
2018-02-04 11:47:44 -0500

    Hemos creado el formulario para realizar la actualización de los datos de los usuarios, en este caso solo vamos a permitir actualizar el nombre y el email.

---
+++ database/actualizar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Actualizar Usuario</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1>Actualizar Usuario</h1>
+		<a href="index.php">Inicio</a>
+
+		<form action="actualizar.php" method="post" accept-charset="utf-8">
+			<label for="text">Nombre</label> <br>
+			<input name="name" type="text" id="text" required> <br><br>
+
+			<label for="email">Email</label> <br>
+			<input name="email" type="email" id="email" required> <br><br>
+
+			<input type="submit" value="Actualizar">
+		</form>
+	</div>
+</body>
+</html>
