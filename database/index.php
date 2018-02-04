ce1a0936637a51cef2ac2dbb499f685141ec838e
Yomar Miranda <mirandayomar@gmail.com>
2018-02-03 23:52:21 -0500

    Hemos reemplazado los valores de la conexión por variables para tener el código mejor organizado.

--- database/index.php
+++ database/index.php
@@ -12,8 +12,13 @@
  * @var PDO
  */
 
+$dbHost = "localhost";
+$dbName = "cursophp";
+$dbUser = "root";
+$dbPass = "root";
+
 try{
-	$pdo = new PDO("mysql:host=localhost;dbname=cursophp", "root", "root");
+	$pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(Exception $e){
 	echo $e->getMessage();
