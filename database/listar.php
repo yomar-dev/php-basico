1463e156c4d53b938c416f877a695fc0ee62da5e
Yomar Miranda <mirandayomar@gmail.com>
2018-02-04 01:24:03 -0500

    Creamos un archivo para mostrar el listado de todos nuestros usuarios pero de momento se encuentra con contenido estatico.

--- database/listar.php
+++ database/listar.php
@@ -3,22 +3,30 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Introdución a DB con PHP</title>
+	<title>Listado de Usuarios</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
 	<div class="container">
-		<h1>Bases de Datos</h1>
+		<h1>Listado de Usuarios</h1>
+		<a href="index.php">Inicio</a>
 
-		<ul>
-			<li>
-				<a href="listar.php">Listar Usuarios</a>
-			</li>
-			<li>
-				<a href="agregar.php">Agregar Usuario</a>
-			</li>
-		</ul>
+		<table class="table">
+			<tr>
+				<th>Nombre</th>
+				<th>Email</th>
+			</tr>
+
+			<tr>
+				<td>Miguel</td>
+				<td>miguel@gmail.com</td>
+			</tr>
+			<tr>
+				<td>Dani</td>
+				<td>dani@gmail.com</td>
+			</tr>
+		</table>
 	</div>
 </body>
 </html>
