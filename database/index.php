4009341ce91ab8ddc626c1ca63bd60cdd932f336
Yomar Miranda <mirandayomar@gmail.com>
2018-02-03 23:20:41 -0500

    Hemos creado una vista sencilla nuestro ejercicio utilizando Bootstrap 3.3.7.

---
+++ database/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Introdución a DB con PHP</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1>Bases de Datos</h1>
+
+		<ul>
+			<li>
+				<a href="#">Listar Usuarios</a>
+			</li>
+			<li>
+				<a href="#">Agregar Usuario</a>
+			</li>
+		</ul>
+	</div>
+</body>
+</html>
