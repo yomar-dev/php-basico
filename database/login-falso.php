60b2e2e6d2d6964cc83af1e3da04bd32e143aab7
Yomar Miranda <mirandayomar@gmail.com>
2018-02-05 21:43:48 -0500

    En este nuevo archivo vamos a realizar pruebas de SQL Injection.

---
+++ database/login-falso.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Introdución a SQL Injection</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1>Login Falso</h1>		
+		<a href="index.php">Inicio</a>
+
+		
+	</div>
+</body>
+</html>
